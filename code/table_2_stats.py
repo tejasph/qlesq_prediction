0e54b2d7df7b5b91e9a26993958ae08963267132
tejasph <tphaterp@gmail.com>
2022-09-10 15:02:38 -0700

    created stats for table 2

---
+++ code/table_2_stats.py
@@ -0,0 +1,97 @@
+# Table_2_stats.py
+import pandas as pd
+import datetime
+import typer
+import os
+from scipy.stats import ttest_ind
+from globals import STAT_RESULTS
+
+def main():
+
+    startTime = datetime.datetime.now()
+
+    if os.path.exists(STAT_RESULTS) == False:
+        os.mkdir(STAT_RESULTS)
+
+    output_path = STAT_RESULTS + '/table_2_stat_grid.csv'
+
+    # Table 2
+
+    # Paths involved
+    STARD_OVER_EVAL = r"C:\Users\Tejas\Documents\qlesq_project\qlesq_prediction\results\evaluations\STARD_OVER_EVAL"
+    STARD_OVER_ENET_EVAL = r"C:\Users\Tejas\Documents\qlesq_project\qlesq_prediction\results\evaluations\STARD_OVER_ENET_EVAL"
+
+    CANBIND_EVAL = r"C:\Users\Tejas\Documents\qlesq_project\qlesq_prediction\results\evaluations\CANBIND_EVAL"
+    CANBIND_ENET_EVAL = r"C:\Users\Tejas\Documents\qlesq_project\qlesq_prediction\results\evaluations\CANBIND_ENET_EVAL"
+
+    stard_over = pd.read_csv(os.path.join(STARD_OVER_EVAL, "STARD_OVER_EVAL_raw_scores.csv"))
+    stard_over_enet = pd.read_csv(os.path.join(STARD_OVER_ENET_EVAL, "STARD_OVER_ENET_EVAL_raw_scores.csv"))
+
+    canbind = pd.read_csv(os.path.join(CANBIND_EVAL, "STARD_CANBIND_EVAL_raw_scores.csv"))
+    canbind_enet = pd.read_csv(os.path.join(CANBIND_ENET_EVAL, "STARD_CANBIND_ENET_EVAL_raw_scores.csv"))
+
+    # cols that we want p-values for
+    bal_acc_1 = ["Dummy_Classification_bal_acc", "Logistic_Regression_bal_acc", "Random_Forest_bal_acc",
+     "Elastic_Net_bal_acc", "KNearest_Neighbors_bal_acc", "Support_Vector_Machine_bal_acc", "Gradient Boosting Classifier_bal_acc"]
+
+    bal_acc_2 = ["Dummy_Classification_bal_acc", "Logistic_Regression_bal_acc", "Random_Forest_bal_acc",
+     "Elastic_Net_bal_acc", "KNearest_Neighbors_bal_acc", "Support_Vector_Machine_bal_acc", "Gradient Boosting Classifier_bal_acc"]
+
+    auc_1 = ["Dummy_Classification_auc", "Logistic_Regression_auc", "Random_Forest_auc",
+     "Elastic_Net_auc", "KNearest_Neighbors_auc", "Support_Vector_Machine_auc", "Gradient Boosting Classifier_auc"]
+
+    auc_2 = ["Dummy_Classification_auc", "Logistic_Regression_auc", "Random_Forest_auc",
+     "Elastic_Net_auc", "KNearest_Neighbors_auc", "Support_Vector_Machine_auc", "Gradient Boosting Classifier_auc"]
+
+
+    f = open(output_path, 'w+')
+    f.write('T_test Grid\n Balanced Accuracy two-tailed two-sided t-tests\n')
+    f.write("Overlapping STAR*D (n = 100) vs CANBIND: Balanced Accuracy and AUC P-Values\n")
+
+    ######################### Overlapping STAR*D vs CANBIND
+
+    f.write("Metric , Balanced Acc P-Value," )
+    for col in bal_acc_1:
+        f.write("\n")
+        f.write(f'{col},')
+        f.write(f'{ttest_ind(stard_over[col], canbind[col]).pvalue},')
+
+    f.write("\n\nMetric , AUC P-Value," )
+    for col in auc_1:
+        f.write("\n")
+        f.write(f'{col},')
+        f.write(f'{ttest_ind(stard_over[col], canbind[col]).pvalue},')
+
+    ########################
+
+    # Remove enet model for next comparisons
+    bal_acc_1.remove("Elastic_Net_bal_acc")
+    bal_acc_2.remove("Elastic_Net_bal_acc")
+    auc_1.remove("Elastic_Net_auc")
+    auc_2.remove("Elastic_Net_auc")
+
+    f.write("\n\nOverlapping STARD ENet (n = 61) vs CANBIND  Balanced Accuracy and AUC P-Values\n\n")
+
+    ######################### Overlapping STAR*D Enet vs CANBIND
+
+    f.write("Metric , Balanced Acc P-Value," )
+    for col in bal_acc_1:
+        f.write("\n")
+        f.write(f'{col},')
+        f.write(f'{ttest_ind(stard_over_enet[col], canbind_enet[col]).pvalue},')
+
+    f.write("\n\nMetric , AUC P-Value," )
+    for col in auc_1:
+        f.write("\n")
+        f.write(f'{col},')
+        f.write(f'{ttest_ind(stard_over_enet[col], canbind_enet[col]).pvalue},')
+
+    ########################
+
+
+    f.close()
+
+    print(f"Completed in: {datetime.datetime.now() - startTime}")
+
+if __name__ == "__main__":
+    typer.run(main)
